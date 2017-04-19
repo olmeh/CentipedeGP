0bf470016da8c64f74fb88e9ce79281ba503f4ff
Olmeh Salinas <voltage016@gmail.com>
2017-04-19 18:49:41 -0500

    Start

---
+++ ProjectTest/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SDL_Plotter.h"
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    SDL_Plotter GameScreen(1000,1000);
+
+    char key;
+
+    while(!GameScreen.getQuit()){
+        if(GameScreen.kbhit()){
+            key = GameScreen.getKey();
+        }
+
+
+    }
+
+    return 0;
+}
