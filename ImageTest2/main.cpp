ed93675dc4b071e4cbebf92a607ab62d3062e3a8
olmeh <voltage016@gmail.com>
2017-04-21 18:04:59 -0500

    images

--- ImageTest2/main.cpp
+++ ImageTest2/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "SDL_Plotter.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int r,g,b;
+
+    fstream file;
+    string fileName;
+
+    do{
+        cout << "Enter File Name: ";
+        cin >> fileName;
+
+        file.open(fileName.c_str());
+
+        if(!file) cout << "File Not Found" << endl;
+    }while(!file);
+
+    SDL_Plotter image(800,800);
+
+    char key;
+
+    for(int i=0; i<800; i++)){
+        for(int j=0; j<800; j++)){
+            image.plotPixel(i,j,0,0,0);
+        }
+    }
+
+    for(int i=0; i<20; i++)){
+        for(int j=0; j<20; j++)){
+            image.plotPixel(i,j,r,g,b);
+        }
+    }
+
+    while(!image.getQuit()){
+        if(image.kbhit()){
+            key = image.getKey();
+        }
+
+
+
+        image.update();
+    }
+
+
+
+
 }
